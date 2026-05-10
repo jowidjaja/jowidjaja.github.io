e5e6d9c24c1a4fa5cd8f08aa9c0a7df637d344f7
jowidjaja <116462552+jowidjaja@users.noreply.github.com>
2026-05-10 16:52:49 -0400

    Create personal.js

---
+++ personal.js
@@ -0,0 +1,76 @@
+//array for gallery
+const gallery = [
+    {
+        image: "images/testpic.png",
+        caption: "this is an example",
+        date: "",
+        location: "",
+        tags: [],
+        hidden: false,
+    },
+    {
+        image: "images/testpic.png",
+        caption: "",
+        date: "",
+        location: "",
+        tags: [],
+        hidden: false,
+
+    },
+    {
+        image: "images/testpic.png",
+        caption: "",
+        date: "",
+        location: "",
+        tags: [],
+        hidden: false,
+
+    },
+    {
+        image: "images/testpic.png",
+        caption: "",
+        date: "",
+        location: "",
+        tags: [],
+        hidden: false,
+    },
+    {
+        image: "images/testpic.png",
+        caption: "",
+        date: "",
+        location: "",
+        tags: [],
+        hidden: false,
+    },
+    {
+        image: "images/testpic.png",
+        caption: "",
+        date: "",
+        location: "",
+        tags: [],
+        hidden: false,
+    },
+    {
+        image: "images/testpic.png",
+        caption: "",
+        date: "",
+        location: "",
+        tags: [],
+        hidden: false,
+    },
+    
+]
+
+const galleryContainer = document.querySelector(".gallery");
+
+gallery.forEach(item => {
+  const card = document.createElement("div");
+  card.classList.add("gallery_item");
+
+  card.innerHTML = `
+    <img src="${item.image}" alt="">
+    ${item.caption ? `<p>${item.caption}</p>` : ""}
+  `;
+
+  galleryContainer.appendChild(card);
+});
